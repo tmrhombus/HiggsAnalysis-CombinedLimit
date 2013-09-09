066ef3a876e097b70e4bde7ce39ee49e8208f1d2
T.M.Perry <tomperry7@gmail.com>
2013-09-09 11:00:05 -0500

    grab Datacards_ from UWAnalysis

---
+++ test/wbb/eight/getDatacards.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp /afs/hep.wisc.edu/cms/tperry/CMSSW_5_3_9/src/UWAnalysis/CRAB/MuNu/plots/Datacards_*root .
+
+cp /afs/hep.wisc.edu/cms/tperry/CMSSW_5_3_9/src/UWAnalysis/CRAB/MuNu/plots/Datacards_*log .

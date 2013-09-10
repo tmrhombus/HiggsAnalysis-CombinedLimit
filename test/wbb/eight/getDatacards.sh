9b9bdbaa7be4ab506e9867d7b97926dd8d5841fe
T.M.Perry <tomperry7@gmail.com>
2013-09-10 04:02:07 -0500

    point to right place

--- test/wbb/eight/getDatacards.sh
+++ test/wbb/eight/getDatacards.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
-cp /afs/hep.wisc.edu/cms/tperry/CMSSW_5_3_9/src/UWAnalysis/CRAB/MuNu/plots/Datacards_*root .
+#!/bin/sh
+# Author: T.M.Perry
+# Moves datacards to this repo
+cp /afs/hep.wisc.edu/cms/tperry/CMSSW_5_3_9/src/UWAnalysis/CRAB/MuNu/plots/Datacard_*root .
 
-cp /afs/hep.wisc.edu/cms/tperry/CMSSW_5_3_9/src/UWAnalysis/CRAB/MuNu/plots/Datacards_*log .
+cp /afs/hep.wisc.edu/cms/tperry/CMSSW_5_3_9/src/UWAnalysis/CRAB/MuNu/plots/Datacard_*log .

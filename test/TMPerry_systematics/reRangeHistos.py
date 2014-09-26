e0cdb485f0afec136470bb7a0d1103d160ac291c
Thomas <tperry@cern.ch>
2014-09-26 18:38:58 -0500

    merged W4F and W5F into single file

--- test/TMPerry_systematics/reRangeHistos.py
+++ test/TMPerry_systematics/reRangeHistos.py
@@ -9,6 +9,31 @@ import ROOT
 from ROOT import TH1F,TFile,gROOT
 import sys
 
+inFilenames=[
+sys.argv[1],sys.argv[2],sys.argv[3]
+]
+
+for inFilename in inFilenames:
+ inFile = TFile(inFilename+'.root')
+
+ outFile=gROOT.FindObject('Rescaled_'+inFilename+'.root')
+ if outFile : outFile.Close()
+ outFile = TFile('Rescaled_'+inFilename+'.root','RECREATE','rescaled histograms')
+ log = open('Rescaled_'+inFilename+'.log','a')
+
+ for key in inFile.GetListOfKeys():
+  obj = key.ReadObj()
+  if(obj.IsA().InheritsFrom("TH1")):
+   h_name = obj.GetName()
+   nBins = obj.GetNbinsX()
+   h_new = TH1F(h_name,h_name,nBins+1,0,nBins+1)
+   for i in xrange(1,nBins+2):
+    h_new.SetBinContent(i,obj.GetBinContent(i))
+   #print h_new.GetName()
+   #print h_new.Integral()
+   outFile.cd()
+   outFile.Write()
+
 def rename(name,old,new):
  if(name==old):
   return new
@@ -22,43 +47,4 @@ def mergeHistos(histos,outname):
  for histo in histos:
   h.Add(histo)
  return h
-
-inFilenames=[
-sys.argv[1],sys.argv[2],sys.argv[3]
-]
-Wbb4Fnames=[
-sys.argv[4],sys.argv[5],sys.argv[6]
-]
-
-for inFilename,Wbb4Fname in zip(inFilenames,Wbb4Fnames):
- inFile = TFile(inFilename+'.root')
- w4fFile = TFile(Wbb4Fname+'.root')
-
- outFile=gROOT.FindObject('Renamed4F_'+inFilename+'.root')
- if outFile : outFile.Close()
- outFile = TFile('Renamed4F_'+inFilename+'.root','RECREATE','renamed histograms')
- log = open('Renamed4F_'+inFilename+'.log','a')
-
- #Wcc_infile = inFile.Get('Wcc')
- #Wc_infile = inFile.Get('Wc')
- #Wcc = mergeHistos([Wcc_infile,Wc_infile],'Wcc')
- #Wcc_infile.Delete()
- #Wc_infile.Delete()
- #Wcc_infile.SetName('Wcc_infile')
-
- for key in inFile.GetListOfKeys():
-  obj = key.ReadObj()
-  if obj.GetName=='Wc' : continue
-  if obj.GetName=='Wcc' : continue
-  if(obj.IsA().InheritsFrom("TH1")):
-   h_name = obj.GetName()
-   h_name = rename(h_name,"Wbb","Wbb5F")
-   nBins = obj.GetNbinsX()
-   h_new = TH1F(h_name,h_name,nBins+1,0,nBins+1)
-   for i in xrange(1,nBins+2):
-    h_new.SetBinContent(i,obj.GetBinContent(i))
-   #print h_new.GetName()
-   #print h_new.Integral()
-   outFile.cd()
-   outFile.Write()
 

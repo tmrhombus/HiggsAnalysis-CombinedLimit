cb091ae32134e4b55b6eac85c50b9a3c5d6dcae4
Thomas <tperry@cern.ch>
2014-09-26 18:28:08 -0500

    iteratively loop through histos, add functions to merge/rename histos

--- test/TMPerry_systematics/reRangeHistos.py
+++ test/TMPerry_systematics/reRangeHistos.py
@@ -1,3 +1,5 @@
+## TO DO: merge Wc and Wcc
+
 #!/usr/bin/env python
 '''
 Resize histograms for 8TeV datacards 
@@ -7,22 +9,50 @@ import ROOT
 from ROOT import TH1F,TFile,gROOT
 import sys
 
-inFilenames =  [
-                'Renamed4F_'+sys.argv[1],
-                'Renamed4F_'+sys.argv[2],
-                'Renamed4F_'+sys.argv[3],
-               ]
-for inFilename in inFilenames:
+def rename(name,old,new):
+ if(name==old):
+  return new
+ else:
+  return name
+
+def mergeHistos(histos,outname):
+ h = histos[0].Clone()
+ h.SetName(outname)
+ histos.pop(0)
+ for histo in histos:
+  h.Add(histo)
+ return h
+
+inFilenames=[
+sys.argv[1],sys.argv[2],sys.argv[3]
+]
+Wbb4Fnames=[
+sys.argv[4],sys.argv[5],sys.argv[6]
+]
+
+for inFilename,Wbb4Fname in zip(inFilenames,Wbb4Fnames):
  inFile = TFile(inFilename+'.root')
- 
- outFile=gROOT.FindObject('ReRanged_'+inFilename+'.root')
+ w4fFile = TFile(Wbb4Fname+'.root')
+
+ outFile=gROOT.FindObject('Renamed4F_'+inFilename+'.root')
  if outFile : outFile.Close()
- outFile = TFile('ReRanged_'+inFilename+'.root','RECREATE','xaxis as bin number')
-  
+ outFile = TFile('Renamed4F_'+inFilename+'.root','RECREATE','renamed histograms')
+ log = open('Renamed4F_'+inFilename+'.log','a')
+
+ #Wcc_infile = inFile.Get('Wcc')
+ #Wc_infile = inFile.Get('Wc')
+ #Wcc = mergeHistos([Wcc_infile,Wc_infile],'Wcc')
+ #Wcc_infile.Delete()
+ #Wc_infile.Delete()
+ #Wcc_infile.SetName('Wcc_infile')
+
  for key in inFile.GetListOfKeys():
   obj = key.ReadObj()
+  if obj.GetName=='Wc' : continue
+  if obj.GetName=='Wcc' : continue
   if(obj.IsA().InheritsFrom("TH1")):
    h_name = obj.GetName()
+   h_name = rename(h_name,"Wbb","Wbb5F")
    nBins = obj.GetNbinsX()
    h_new = TH1F(h_name,h_name,nBins+1,0,nBins+1)
    for i in xrange(1,nBins+2):
@@ -31,4 +61,4 @@ for inFilename in inFilenames:
    #print h_new.Integral()
    outFile.cd()
    outFile.Write()
-  
+
